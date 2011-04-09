4edb138e8970995c583f72a7dd4fcacaf9f0b440
Michael Santos <michael.santos@gmail.com>
2011-04-09 18:28:50 -0400

    Add iwconfig functionality
    Add support for getting and setting wireless parameters, similar to
    iwconfig. Setting values is untested.

--- ebin/wierl.app
+++ ebin/wierl.app
@@ -3,7 +3,8 @@
     {description, "Erlang interface for manipulating 802.11 devices"},
     {vsn, "0.01"},
     {modules, [
-        wierl_scan
+        wierl_scan,
+        wierl_config
             ]},
     {registered, []},
     {applications, [
--- src/wierl_scan.erl
+++ src/wierl_scan.erl
@@ -35,12 +35,33 @@
         decode/1
     ]).
 
--include("wierl_scan.hrl").
+-include("wierl.hrl").
 
 -record(state, {
         ap,
         aps
     }).
+
+-define(IW_MAX_BITRATES, 32).
+-define(IW_MAX_ENCODING_SIZES, 8).
+-define(IW_MAX_TXPOWER, 8).
+
+-define(IW_POWER_ON, 16#0000).
+-define(IW_POWER_TYPE, 16#F000).
+-define(IW_POWER_PERIOD, 16#1000).
+-define(IW_POWER_TIMEOUT, 16#2000).
+-define(IW_POWER_SAVING, 16#4000).
+-define(IW_POWER_MODE, 16#0F00).
+-define(IW_POWER_UNICAST_R, 16#0100).
+-define(IW_POWER_MULTICAST_R, 16#0200).
+-define(IW_POWER_ALL_R, 16#0300).
+-define(IW_POWER_FORCE_S, 16#0400).
+-define(IW_POWER_REPEATER, 16#0800).
+-define(IW_POWER_MODIFIER, 16#000F).
+-define(IW_POWER_MIN, 16#0001).
+-define(IW_POWER_MAX, 16#0002).
+-define(IW_POWER_RELATIVE, 16#0004).
+
 
 
 %%
@@ -233,13 +254,15 @@ decode({Key, List}) when is_list(List) ->
 
 % What is 1?
 decode({essid, <<Len:?UINT16, 1:?UINT16, Bin/binary>>}) ->
-    <<ESSID:Len/bytes>> = Bin,
+    <<ESSID:Len/bytes, _/binary>> = Bin,
     ESSID;
-decode({bssid, <<1,0, Bytes:6/bytes, 0,0,0,0,0,0,0,0>>}) ->
+decode({bssid, <<1,0, Bytes:6/bytes, 0,0,0,0,0,0,0,0, _/binary>>}) ->
     lists:flatten(string:join([ io_lib:format("~.16b", [N]) || <<N:8>> <= Bytes ], ":"));
+decode({ap, AP}) ->
+    decode({bssid, AP});
 decode({mode, <<Mode:?UINT32>>}) ->
     mode(Mode);
-decode({qual, <<Qual:8, Level:8/signed, Noise:8, Updated:8>>}) ->
+decode({qual, <<Qual:8, Level:8/signed, Noise:8, Updated:8, _/binary>>}) ->
     [{quality, Qual}, {level, Level}, {noise, Noise}, {updated, decode({updated, Updated})}];
 
 decode({updated, Status}) when Status band ?IW_QUAL_QUAL_UPDATED == 1 ->
@@ -247,10 +270,119 @@ decode({updated, Status}) when Status band ?IW_QUAL_QUAL_UPDATED == 1 ->
 decode({updated, _Status}) ->
     false;
 
-decode({freq, <<Channel:?UINT64>>}) when Channel < 1000 ->
+decode({freq, <<Channel:?UINT64, _/binary>>}) when Channel < 1000 ->
     {channel, Channel};
-decode({freq, <<M:?INT32, E:?INT16, _I:8, _Flags:8>>}) ->
+decode({freq, <<M:?INT32, E:?INT16, _I:8, _Flags:8, _/binary>>}) ->
     {frequency, M*math:pow(10, E)};
+
+decode({power, Power}) ->
+    decode({param, Power});
+
+decode({param, <<Value:?INT32, Fixed:8, Disabled:8, Flags:?UINT32, _/binary>>}) ->
+    [{value, Value}, {fixed, Fixed}, {disabled, Disabled}, {flags, Flags}];
+
+decode({range, <<
+        Throughput:?UINT32,
+        Min_nwid:?UINT32,
+        Max_nwid:?UINT32,
+        Old_num_channels:?UINT16,
+        Old_num_frequency:8,
+        Scan_capa:8,
+        Event_capa:(6*4)/bytes,
+        Sensitivity:?INT32,
+        Max_qual:4/bytes,
+        Avg_qual:4/bytes,
+        Num_bitrates:8,
+        Bitrate:(?IW_MAX_BITRATES*4)/bytes,
+        Min_rts:?INT32,
+        Max_rts:?INT32,
+        Min_frag:?INT32,
+        Max_frag:?INT32,
+        Min_pmp:?INT32,
+        Max_pmp:?INT32,
+        Min_pmt:?INT32,
+        Max_pmt:?INT32,
+        Pmp_flags:?UINT16,
+        Pmt_flags:?UINT16,
+        Pm_capa:?UINT16,
+        Encoding_size:(?IW_MAX_ENCODING_SIZES*2)/bytes,
+        Num_encoding_sizes:8,
+        Max_encoding_tokens:8,
+        Encoding_login_index:8,
+        Txpower_capa:?UINT16,
+        Num_txpower:8,
+        Txpower:(?IW_MAX_TXPOWER*4)/bytes,
+        We_version_compiled:8,
+        We_version_source:8,
+        Retry_capa:?UINT16,
+        Retry_flags:?UINT16,
+        R_time_flags:?UINT16,
+        Min_retry:?INT32,
+        Max_retry:?INT32,
+        Min_r_time:?INT32,
+        Max_r_time:?INT32,
+        Num_channels:?UINT16,
+        Num_frequency:8,
+        Freq:(?IW_MAX_FREQUENCIES*8)/bytes,
+        Enc_capa:?UINT32,
+        Min_pms:?INT32,
+        Max_pms:?INT32,
+%        Pms_flags:?UINT16,
+%        Modul_capa:?INT32,
+%        Bitrate_capa:?UINT32,
+        _/binary
+        >>}) ->
+    [
+        {throughput, Throughput},
+        {min_nwid, Min_nwid},
+        {max_nwid, Max_nwid},
+        {old_num_channels, Old_num_channels},
+        {old_num_frequency, Old_num_frequency},
+        {scan_capa, Scan_capa},
+        {event_capa, Event_capa},
+        {sensitivity, Sensitivity},
+        {max_qual, decode({qual, Max_qual})},
+        {avg_qual, decode({qual, Avg_qual})},
+        {num_bitrates, Num_bitrates},
+        {bitrate, Bitrate},
+        {min_rts, Min_rts},
+        {max_rts, Max_rts},
+        {min_frag, Min_frag},
+        {max_frag, Max_frag},
+        {min_pmp, Min_pmp},
+        {max_pmp, Max_pmp},
+        {min_pmt, Min_pmt},
+        {max_pmt, Max_pmt},
+        {pmp_flags, Pmp_flags},
+        {pmt_flags, Pmt_flags},
+        {pm_capa, Pm_capa},
+        {encoding_size, Encoding_size},
+        {num_encoding_sizes, Num_encoding_sizes},
+        {max_encoding_tokens, Max_encoding_tokens},
+        {encoding_login_index, Encoding_login_index},
+        {txpower_capa, Txpower_capa},
+        {num_txpower, Num_txpower},
+        {txpower, Txpower},
+        {we_version_compiled, We_version_compiled},
+        {we_version_source, We_version_source},
+        {retry_capa, Retry_capa},
+        {retry_flags, Retry_flags},
+        {r_time_flags, R_time_flags},
+        {min_retry, Min_retry},
+        {max_retry, Max_retry},
+        {min_r_time, Min_r_time},
+        {max_r_time, Max_r_time},
+        {num_channels, Num_channels},
+        {num_frequency, Num_frequency},
+        {freq, decode({freq, Freq})},
+        {enc_capa, Enc_capa},
+        {min_pms, Min_pms},
+        {max_pms, Max_pms}
+%        {max_pms, Max_pms},
+%        {pms_flags, Pms_flags},
+%        {modul_capa, Modul_capa},
+%        {bitrate_capa, Bitrate_capa}
+    ];
 
 decode({_Key, Val}) ->
     Val.
